6c298d4bce91c6bd75bb1035271a79210a318752
Aman Kumar Maurya <amankrmj@gmail.com>
2025-08-11 16:47:56 +0530

    update: added the ascii art

--- src/main/java/com/amankrmj/capyverse/Main.java
+++ src/main/java/com/amankrmj/capyverse/Main.java
@@ -21,6 +21,12 @@ public class Main implements Runnable {
     @Option(names = {"-v", "--verbose"}, description = "Enable verbose output")
     private boolean verbose = false;
 
+    @Option(names = {"-m", "--mascot"}, description = "Show CapyVerse mascot")
+    private boolean showMascot = false;
+
+    @Option(names = {"-a", "--available"}, description = "Show available commands")
+    private boolean availableCommands = false;
+
     public static void main(String[] args) {
         CommandLine cmd = new CommandLine(new Main());
         int exitCode = cmd.execute(args);
@@ -34,60 +40,60 @@ public class Main implements Runnable {
     @Override
     public void run() {
         System.out.println(
-                "                      .---.                           .---..\n" +
-                        "                    ...  .-..   ............        ...    ...\n" +
-                        "                    .-    ----...................  .-.      .-\n" +
-                        "                       .--.                     .--..   ..  .-\n" +
-                        "                     .-.                              .--.  ..\n" +
-                        "                  ....                                 .  ...\n" +
-                        "                ...                                     .--.\n" +
-                        "               -.                                       ..-.\n" +
-                        "             .-                          .---            .--.\n" +
-                        "           ...  ..........              -++.--             -.\n" +
-                        "          .-   .----------..            ------             ...\n" +
-                        "         -.    ------------.             .--.               .-\n" +
-                        "        ...    ------------.                                .-\n" +
-                        "       .-       ...---....                                  .-.\n" +
-                        "       .-          ---                                      .--.\n" +
-                        "       .-.         ---                                      .--.\n" +
-                        "       .-          ---                                      .--.\n" +
-                        "       .-.         ---                                      .--.\n" +
-                        "         ..        ----.                                    .--.\n" +
-                        "         -.     .--.   .--                                  .--.\n" +
-                        "          .-                                                .--.\n" +
-                        "           ...                   ..                         .--.\n" +
-                        "             .--.              .-.                           .-.\n" +
-                        "              .----------------.                             .-..\n" +
-                        "          ...--    .........                                  .--\n" +
-                        "         ....--                                                .-\n" +
-                        "       .-.   --                                                .--.\n" +
-                        "     ...    -.                                    .-            .-.\n" +
-                        "   ...    ...                                    ...             ...\n" +
-                        "   .-    .--                                    .-.       .       --\n" +
-                        "   .-  .----                                     -.      .-.      .--.\n" +
-                        "   ...----.                                      -.     ...         -.\n" +
-                        "     ....-.                                      --.    --          -.\n" +
-                        "         -.                                       .- .--.           --.\n" +
-                        "         -.                                       .---.              -.\n" +
-                        "         -.                                                          ..\n" +
-                        "         -.                                                          -.\n" +
-                        "         --                                                          ..\n" +
-                        "         .--                                                        .-.\n" +
-                        "          .-.                                                       --.\n" +
-                        "           .-.                                                    .--.\n" +
-                        "             .-                                                   --\n" +
-                        "             .--.                                               .--.\n" +
-                        "               --....                                          ...\n" +
-                        "               -. ...--.                                      ...\n" +
-                        "               -.      .------------------------..           .-.\n" +
-                        "              ...         -.                     -..         ...\n" +
-                        "             .-          ...                     .--        .-\n" +
-                        "  ---------------------------------------------------------------------.                         \n"
+                """
+                         _   _    ____                   __     __                   _   _\s
+                        | | | |  / ___|__ _ _ __  _   _  \\ \\   / /__ _ __ ___  ___  | | | |
+                        | | | | | |   / _` | '_ \\| | | |  \\ \\ / / _ \\ '__/ __|/ _ \\ | | | |
+                        |_| |_| | |__| (_| | |_) | |_| |   \\ V /  __/ |  \\__ \\  __/ |_| |_|
+                        (_) (_)  \\____\\__,_| .__/ \\__, |    \\_/ \\___|_|  |___/\\___| (_) (_)
+                                           |_|    |___/                                   \s"""
         );
+        System.out.println();
+        if (showMascot) {
+            capyMascot();
+        }
         System.out.println("CapyVerse - Complete Development Environment Manager");
         System.out.println("Use --help for options or try these commands:");
-        System.out.println("  path     - Manage environment PATH variables");
         System.out.println("  java     - Manage Java versions and installations");
-        System.out.println("  native   - Manage native compilation and cross-platform builds");
+
+    }
+
+    private void capyMascot() {
+        System.out.println("""
+                                                          .' '.                 -' '-                        \s
+                                                         .-   ---..........-.  -     -                       \s
+                                                          '..'               '.'..- -                       \s
+                                                        .-'                     '. ..                        \s
+                                                      .-                          --                         \s
+                                                    .-                  --.        --                        \s
+                                                   -   .-----.         -+--         -                        \s
+                                                  -.  --------.        .--.          -                       \s
+                                                 ..   .-------.         ''           -                       \s
+                                                 -     ' --- '                       --                      \s
+                                                 -       --                          --                      \s
+                                                 -       -.                          --                      \s
+                                                  -      --                          --                      \s
+                                                  ..   .'  '.                        --                      \s
+                                                   ..               .'               --                      \s
+                                                     .'.         .-'                 --                      \s
+                                                    .-  '-.....-'                     --                     \s
+                                                  - .-                                 --                    \s
+                                                -   -                         -         -                    \s
+                                              -    -                         -           -                   \s
+                                             -    --                         -     -     -.                  \s
+                                             '..- -                          -    .       -                  \s
+                                                  -                          '.  .        -.                 \s
+                                                  -                            ''          -                 \s
+                                                  -                                        -                 \s
+                                                  -                                        -                 \s
+                                                  .-                                      .-                 \s
+                                                   .-                                    .-                  \s
+                                                    .-.                                 .-                   \s
+                                                    --                                  -                    \s
+                                                     -   .--    ------------           -                     \s
+                                                      -       .'            '.        -                      \s
+                                                       '-   -'                '-    -'                       \s
+                                             ................................................\
+                """);
     }
 }
